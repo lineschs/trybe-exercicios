52828b779fa315eabf8263cb8c4e420a8ec086d2
lineschs <line.rsanches@gmail.com>
2022-05-18 09:53:29 -0300

    Exercício 4.1.1

---
+++ fundamentos/bloco-4-intro-java-e-logica/dia-1-primeiros-passos/scripts.js
@@ -0,0 +1,10 @@
+const myName= "Aline";
+const birthCity = "Araraquara";
+// birthCity = "Miami";
+
+let birthYear = 1991;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
