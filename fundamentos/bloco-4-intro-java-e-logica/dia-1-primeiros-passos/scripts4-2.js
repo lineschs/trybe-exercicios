f6a4014640299884e9aa907312510a7402b7bfc6
lineschs <line.rsanches@gmail.com>
2022-05-31 19:15:45 -0300

    4-2 primeiros exs

---
+++ fundamentos/bloco-4-intro-java-e-logica/dia-1-primeiros-passos/scripts4-2.js
@@ -0,0 +1,8 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+    menu.push('Contato')
+console.log(menu);
